4e2d90ef33c5342a1f4517c237f7a0bebf2e8969
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-21 11:07:42 +0530

    Added: board created

--- src/com/tictactoemain/TicTacToeMain.java
+++ src/com/tictactoemain/TicTacToeMain.java
@@ -2,9 +2,22 @@ package com.tictactoemain;
 
 public class TicTacToeMain 
 {
+    public static char board[] = new char[10]; //Array of Character size 10
+    
+    //Creating TicTacToe Board
+    public static char[] createBoard()
+    {
+        for(int i=1; i<board.length; i++)
+        {
+            board[i] = ' '; //Intialize each elements with Empty Space
+        }
+        return board;
+    }
     //Main Method
     public static void main(String[] args) 
     {
         System.out.println("+-+-+ WELCOME TO TIC TAC TOE GAME +-+-+");
+        
+        char storeBoard[] = createBoard(); //Calling the Create Board
     }
 }
