e6e894da690c79c98dc88ba8ed7e88cd5cef70f6
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-21 11:12:47 +0530

    Added: choose x or o letter

--- src/com/tictactoemain/TicTacToeMain.java
+++ src/com/tictactoemain/TicTacToeMain.java
@@ -1,5 +1,7 @@
 package com.tictactoemain;
 
+import java.util.Scanner;
+
 public class TicTacToeMain 
 {
     public static char board[] = new char[10]; //Array of Character size 10
@@ -13,11 +15,29 @@ public class TicTacToeMain
         }
         return board;
     }
+    
+    //Choose the X or O letter 
+    public static void chooseLetter()
+    {
+        Scanner scanInput = new Scanner(System.in);
+        System.out.print("Choose small letter 'x' or 'o' : ");
+        char userInput = scanInput.next().charAt(0); //taking input from user
+        if(userInput == 'x' || userInput == 'o')
+        {
+            userInput = ( userInput == 'x')? 'x' : 'o';  //checking the condition
+        }
+        else
+        {
+            System.out.println("\n!!!Select x or o only!!!\n");
+        }
+        System.out.println("Selected letter is -> "+userInput);
+    }
     //Main Method
     public static void main(String[] args) 
     {
         System.out.println("+-+-+ WELCOME TO TIC TAC TOE GAME +-+-+");
         
         char storeBoard[] = createBoard(); //Calling the Create Board
+        chooseLetter(); //choose the values x and o
     }
 }
